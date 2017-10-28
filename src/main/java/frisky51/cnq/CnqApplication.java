015ba676c4980197866c663bf4b9303e15eacb83
EDupre13 <EDupre13@gmail.com>
2017-10-28 15:19:23 -0500

    first commit

---
+++ src/main/java/frisky51/cnq/CnqApplication.java
@@ -0,0 +1,12 @@
+package frisky51.cnq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnqApplication.class, args);
+	}
+}
